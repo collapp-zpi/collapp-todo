23f96657030190970be317f8b2d14156b209f3db
Karol Sitarz <ka.sitarz@gmail.com>
2021-11-20 00:58:39 +0100

    initial commit

--- src/plugin/components/client.jsx
+++ src/plugin/components/client.jsx
@@ -1,28 +1,80 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { FiCheck, FiPlus, FiTrash } from "react-icons/fi";
+import classNames from "classnames";
+import styled from "styled-components";
 import { useWebsockets } from "../../core/useWebsockets";
-import { CgSpinner } from "react-icons/cg";
 
-function Plugin({ websockets, ids, size }) {
-  const { loading, connected, socket, state, send, errors, room, functions } =
-    useWebsockets(`${websockets}/?id=${ids.space}_${ids.plugin}`);
+const StyledContainer = styled.div`
+  background-size: 2rem 2rem;
+  background-position: -1em -1em;
+  background-image:
+    linear-gradient(to right, rgba(0, 0, 0, 0.03) 1px, transparent 1px),
+    linear-gradient(to bottom, rgba(0, 0, 0, 0.03) 1px, transparent 1px);
+`
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+function Plugin({ websockets, ids }) {
+  const { state, send } =
+    useWebsockets(`${websockets}/?id=${ids?.space}_${ids?.plugin}`);
+
+  const todos = state?.todos ?? []
+
+  const handleUpdate = (i) => (data) => {
+    send("update", { i, ...data })
+  }
+
+  const handleDelete = (i) => () => {
+    send("remove", i)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let value = e?.target?.todo?.value
+    if (!value) return
+    value = String(value).trim()
+    if (!value) return
+
+    send("create", { value })
+    e.target.todo.value = ''
+  }
+
+  const handleClick = (i) => () => {
+    handleUpdate(i)({ isDone: !todos[i].isDone })
+  }
+
+  const handleChange = (i) => (e) => {
+    const value = e?.target?.value
+    if (!value) return
+
+    handleUpdate(i)({ value })
+  }
 
   return (
-    <div className="w-full h-full bg-blueGray-800 text-white text-center py-12 flex justify-center justify-items-center flex-col space-y-8">
-      {!loading ? (
-        <div className="space-y-6">
-          <h1 className="text-3xl text-emerald-400 text-center m-auto font-extrabold">
-            Connected
-          </h1>
-          <h3 className="text-sm text-white">State: {JSON.stringify(state)}</h3>
+    <StyledContainer className="w-full h-full bg-gray-100 p-8 overflow-y-auto space-y-0.5 text-gray-500">
+      {todos.map(({ isDone, value }, i) => (
+        <div className="flex select-none">
+          <div className="flex cursor-pointer bg-black bg-opacity-0 hover:bg-opacity-5 transition-colors rounded-xl py-4 px-2" onClick={handleClick(i)}>
+            <div className={classNames(`w-6 h-6 rounded-md border-blue-500 border-2 flex-shrink-0 relative overflow-hidden text-blue-500 transition-colors`, isDone && 'bg-blue-500')}>
+              <FiCheck className={classNames("text-white absolute left-1/2 top-1/2 transition-transform -translate-y-1/2 -translate-x-1/2", isDone ? 'scale-100' : 'scale-0')} strokeWidth={3} />
+            </div>
+          </div>
+          <input className="py-4 px-2 flex-grow bg-transparent outline-none" value={value} onChange={handleChange(i)} />
+          <div className="flex group items-center cursor-pointer bg-black bg-opacity-0 hover:bg-opacity-5 transition-colors rounded-xl p-2 ml-1" onClick={handleDelete(i)}>
+            <div className="flex items-center justify-center font-bold flex-shrink-0 opacity-20 text-red-500 group-hover:opacity-100 transition-opacity">
+              <FiTrash />
+            </div>
+          </div>
         </div>
-      ) : (
-        <CgSpinner className="animate-spin text-gray-500 text-3xl m-auto" />
-      )}
-    </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <label className="flex items-center relative">
+          <div className="w-6 h-6 my-4 mx-2">
+            <FiPlus className="text-2xl" strokeWidth={3} />
+          </div>
+          <input className="py-4 px-2 flex-grow bg-transparent outline-none" name="todo" placeholder="New task" />
+        </label>
+      </form>
+    </StyledContainer>
   );
 }
 
--- src/plugin/logic/server.js
+++ src/plugin/logic/server.js
@@ -1,19 +1,52 @@
-const set = (state, data) => {
+const create = (state, { value }) => {
+  const todos = state?.todos ?? []
   return {
     ...state,
-    arr: [...(state.arr || []), data.item],
+    todos: [
+      ...todos,
+      {
+        isDone: false,
+        value,
+      },
+    ],
   };
 };
 
-const open = (state, data) => {
+const update = (state, { i, ...data }) => {
+  const todos = state?.todos ?? []
+  return {
+    ...state,
+    todos: [
+      ...todos.slice(0, i),
+      {
+        ...todos[i],
+        ...data,
+      },
+      ...todos.slice(i + 1),
+    ],
+  };
+};
+
+const remove = (state, i) => {
+  const todos = state?.todos ?? []
+  return {
+    ...state,
+    todos: [
+      ...todos.slice(0, i),
+      ...todos.slice(i + 1),
+    ],
+  };
+};
+
+const open = (state) => {
   return state;
 };
 
 const events = {
-  set,
+  create,
+  update,
+  remove,
   __OPEN: open,
 };
 
-module.exports = {
-  default: events,
-};
+export default events;
